830157ff177094a700e35a19e4820ba6f7fd656e
laibinglin <laibinglin@git.com>
2024-09-06 20:07:45 +0800

    init

--- src/main/java/org/lai/springboot/config/EmailProperties.java
+++ src/main/java/org/lai/springboot/config/EmailProperties.java
@@ -1,9 +1,22 @@
 package org.lai.springboot.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author: zurichscud
  * @Date: 2024/9/5 下午11:09
  * @Description: TODO
  */
+
+@ConfigurationProperties(value = "email")
+@Component
+@Data
 public class EmailProperties {
+    private String username;
+    private String password;
+    private String host;
+    private String port;
 }
--- src/test/java/org/lai/springboot/SpringbootPracticeApplicationTests.java
+++ src/test/java/org/lai/springboot/SpringbootPracticeApplicationTests.java
@@ -1,13 +1,19 @@
 package org.lai.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.lai.springboot.config.EmailProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class SpringbootPracticeApplicationTests {
 
+    @Resource
+    private EmailProperties emailProperties;
     @Test
     void contextLoads() {
+        System.out.println(emailProperties);
     }
 
 }
